43b823b5813251b232c092b8b095f9c429052172
hugotellez <hugo-tg91@hotmail.com>
2019-04-28 09:24:36 +0100

    Pagina web responsive

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# CursoPlatzi-Responsive-Design
+# CursoPlatzi-Responsive-Design1
 Pagina web responsive realizada en clases de Platzi
---
+++ js/javascript.js
@@ -0,0 +1,29 @@
+const menu = document.querySelector('.menu')
+const burgerButton = document.querySelector('#burger-button')
+
+const ipad = window.matchMedia('screen and (max-width: 767px)')
+ipad.addListener(validation)
+
+
+
+
+
+function validation(event){
+    if(event.matches)
+        burgerButton.addEventListener('click', hideShow)
+    else
+        burgerButton.removeEventListener('click', hideShow)
+   
+}
+
+    validation(ipad)
+
+
+function hideShow(){
+    if(menu.classList.contains('is-active')){
+        menu.classList.remove('is-active')
+
+    }else{
+        menu.classList.add('is-active')
+    }
+}
